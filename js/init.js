4462ed2fc840d8406288b3436a31938c76fad4a6
Camille Pascua <cpascua78@gmail.com>
2020-03-28 03:46:08 -0400

    added carousel and took out section

--- js/init.js
+++ js/init.js
@@ -6,3 +6,16 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+//for carousel
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.carousel').carousel();
+});
